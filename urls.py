5483e41c67da56b02630ccc98cd3f292b6395142
Peter Hicks <database404@gmail.com>
2011-08-20 22:01:37 +0100

    Made the base URL work.

--- urls.py
+++ urls.py
@@ -3,7 +3,8 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns('storyteller.views',
+    (r'^$', 'index'),                  
     (r'^storyteller/', include('storyteller.urls')),
     (r'^admin/', include(admin.site.urls)),
 )
